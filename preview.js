0cd2e9f02fb1699bfbdd05315da3bffec65b78b6
eveningwater <854806732@qq.com>
2024-12-01 14:20:46 +0000

    Updates

--- preview.js
+++ preview.js
@@ -1,15 +1,29 @@
-
+const isValidImage = (src) => {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+  });
+};
 const preview = (imgList) => {
-    const viewer = new ewViewer({ img: [...imgList] })
-    imgList.forEach((item, index) => {
-        item.onclick = function () {
-            viewer.viewImage(index)
-        }
-    })
-}
+  const viewer = new ewViewer({ img: imgList });
+  imgList.forEach((item, index) => {
+    item.onclick = function () {
+      viewer.viewImage(index);
+    };
+  });
+};
 window.onload = () => {
-    const imgList = document.querySelectorAll('#VPContent img');
-    if(imgList.length){
-        preview(imgList);
+  const imgList = document.querySelectorAll("#VPContent img");
+  Promise.allSettled(
+    [...imgList].map(item => isValidImage(item?.getAttribute("src")))
+  ).then((res) => {
+    const isLoadSuccess =
+      res.filter((item) => item.status === "fulfilled").length ===
+      imgList.length;
+    if (isLoadSuccess) {
+      preview([...imgList]);
     }
-}
+  });
+};
