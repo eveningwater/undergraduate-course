ff6377f301fa766b86feca1647d9011b121f2fef
eveningwater <854806732@qq.com>
2024-12-01 14:01:34 +0000

    Updates

---
+++ preview.js
@@ -0,0 +1,15 @@
+
+const preview = (imgList) => {
+    const viewer = new ewViewer({ img: imgList })
+    imgList.forEach((item, index) => {
+        item.onclick = function () {
+            viewer.viewImage(index)
+        }
+    })
+}
+window.onload = () => {
+    const imgList = document.querySelectorAll('#VPContent img');
+    if(imgList.length){
+        preview(imgList);
+    }
+}
